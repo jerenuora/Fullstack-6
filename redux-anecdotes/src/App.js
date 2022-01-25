3a5cfafc92d40d79f708bc5e82ba105fcdfe38c9
Jere Nuora <jere.nuora@gmail.com>
2022-01-25 23:43:35 +0200

    6.3, 6.4

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
+import reducer from './reducers/anecdoteReducer'
 
 const App = () => {
   const anecdotes = useSelector(state => state)
@@ -7,6 +8,26 @@ const App = () => {
 
   const vote = (id) => {
     console.log('vote', id)
+    dispatch({
+      type: 'VOTE',
+      data:{ id }
+    })
+  }
+  const generateId = () =>
+  Number((Math.random() * 1000000).toFixed(0))
+
+  const addAnecdote = (event) => {
+    event.preventDefault()
+    const content = event.target.anecdote.value
+    event.target.anecdote.value = ''
+    dispatch({
+      type: 'NEW_ANECDOTE',
+      data: {
+        content,
+        votes: 0,
+        id: generateId()
+      }
+    })
   }
 
   return (
@@ -24,9 +45,9 @@ const App = () => {
         </div>
       )}
       <h2>create new</h2>
-      <form>
-        <div><input /></div>
-        <button>create</button>
+      <form onSubmit={addAnecdote}>
+        <div><input name='anecdote' /></div>
+        <button type='submit'>create</button>
       </form>
     </div>
   )
--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -22,8 +22,25 @@ const initialState = anecdotesAtStart.map(asObject)
 const reducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
+  switch(action.type) {
+    case 'NEW_ANECDOTE':
+      return [...state, action.data]
+    case 'VOTE':
+      const id = action.data.id
+      console.log(action.data.id)
+      const anecdoteToVote = state.find(a => a.id === id)
+      console.log(anecdoteToVote)
+      const votedAnecdote = {
+        ...anecdoteToVote,
+        votes: anecdoteToVote.votes +1 
+      }
+      return state.map(anecdote =>
+        anecdote.id !== id ? anecdote : votedAnecdote 
+      ) 
+    default:
+      return state
 
-  return state
+  }
 }
 
 export default reducer
