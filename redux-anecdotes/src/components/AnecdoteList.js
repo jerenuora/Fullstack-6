e15aba5098b66876d8538dec92e84169f47cb36c
Jere Nuora <jere.nuora@gmail.com>
2022-01-30 15:46:39 +0200

    6.10

--- redux-anecdotes/src/components/AnecdoteList.js
+++ redux-anecdotes/src/components/AnecdoteList.js
@@ -4,7 +4,7 @@ import { newVote } from '../reducers/anecdoteReducer'
 
 
 const AnecdoteList = (props) => {
-    const anecdotes = useSelector(state => state)
+    const anecdotes = useSelector(state => state.anecdotes)
     anecdotes.sort((a, b) => b.votes - a.votes )
     
     const dispatch = useDispatch()
