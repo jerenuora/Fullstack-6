2f2f106d039fe57e61a7b2568810bac8ab467e9c
Jere Nuora <jere.nuora@gmail.com>
2022-02-02 18:35:53 +0200

    cleanup

--- redux-anecdotes/src/components/AnecdoteList.js
+++ redux-anecdotes/src/components/AnecdoteList.js
@@ -1,24 +1,20 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { newVote } from '../reducers/anecdoteReducer'
-import { notificationSetter, notificationClearer } from '../reducers/notificationReducer'
+import { notificationSetter } from '../reducers/notificationReducer'
 
 const AnecdoteList = (props) => {
     const anecdotes = useSelector(state => state.anecdotes)
-    console.log('anecdotes', anecdotes)
     anecdotes.sort((a, b) => b.votes - a.votes )
     const filterWord = useSelector(state => state.filterWord)
     const anecdotesToShow = anecdotes.filter(
         a => a.content.toLowerCase()
         .indexOf(filterWord.toLowerCase()) > -1)
 
-    console.log(filterWord)
-    
     const dispatch = useDispatch()
     const vote = (anecdote) => {
         dispatch(newVote(anecdote))
         dispatch(notificationSetter(`Voted ${anecdote.content}`, 5))
-
     }
 
     return (
@@ -32,11 +28,8 @@ const AnecdoteList = (props) => {
                 <button onClick={() => vote(anecdote)}>vote</button>
             </div>
             </div>
-
         )
-    
     )
-
 }
 
 export default AnecdoteList
